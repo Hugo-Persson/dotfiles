13e0c71e6739f0be5201327298c0e37643f2d477
Hugo Persson <hugo.e.persson@gmail.com>
2025-04-07 21:47:42 +0200

    better shared setup

--- dotfile-management/setup/shared.sh
+++ dotfile-management/setup/shared.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
+
 echo "Setting shell to fish..."
-if ! grep -q "/home/linuxbrew/.linuxbrew/bin/fish" /etc/shells; then
-  echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
-  chsh -s /opt/homebrew/bin/fish
+if [[ "$(uname)" == "Darwin" ]]; then
+  # macOS
+  if ! grep -q "/opt/homebrew/bin/fish" /etc/shells; then
+    echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
+    chsh -s /opt/homebrew/bin/fish
+  fi
+else
+  # Linux
+  if ! grep -q "/home/linuxbrew/.linuxbrew/bin/fish" /etc/shells; then
+    echo "/home/linuxbrew/.linuxbrew/bin/fish" | sudo tee -a /etc/shells
+    chsh -s /home/linuxbrew/.linuxbrew/bin/fish
+  fi
 fi
 
 if ! command -v omf &>/dev/null; then
@@ -11,11 +21,21 @@ if ! command -v omf &>/dev/null; then
 fi
 
 echo "Installing tpm"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+  echo "tpm already installed"
+fi
 
-omf install https://github.com/jhillyerd/plugin-git
-npm install -g @microsoft/compose-language-service
+# Check if git plugin is already installed
+if ! omf list | grep -q "git"; then
+  echo "Installing git plugin for fish..."
+  omf install https://github.com/jhillyerd/plugin-git
+else
+  echo "Git plugin already installed"
+fi
+pnpm install -g @microsoft/compose-language-service
 
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+#curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 
 gh extension install github/gh-copilot
