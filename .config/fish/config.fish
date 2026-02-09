f788b6a7ad523c9fae9207ccf391cd72efe47ba4
Hugo Persson <hugo.e.persson@gmail.com>
2026-02-09 09:52:58 +0100

    feat: sdkman

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,5 +10,8 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 # Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
+# This won't be added again if you remove it.A
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# SDKMAN (handled by reitzig/sdkman-for-fish plugin)
+set -gx SDKMAN_DIR $HOME/.sdkman
